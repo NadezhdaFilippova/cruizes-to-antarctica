02fe18a5e984719fd7a83b9d6520da688fba4fc1
NadezhdaFilippova <n9685232364@yandex.ru>
2022-12-06 22:39:06 +0300

    corrections before test

--- source/js/modules/open-menu.js
+++ source/js/modules/open-menu.js
@@ -1,5 +1,6 @@
-let headerNav = document.querySelector('.head-container');
-let navToggle = document.querySelector('.main-nav__toggle');
+const headerNav = document.querySelector('.head-container');
+const navToggle = document.querySelector('.main-nav__toggle');
+const body = document.querySelector('body');
 
 
 const onNavToggle = ()=> {
@@ -10,12 +11,23 @@ const onNavToggle = ()=> {
       if (headerNav.classList.contains('is-closed')) {
         headerNav.classList.remove('is-closed');
         headerNav.classList.add('is-opened');
+        body.classList.add('scroll-lock');
       } else {
         headerNav.classList.add('is-closed');
         headerNav.classList.remove('is-opened');
+        body.classList.remove('scroll-lock');
       }
     });
   }
 };
+
+const menu = document.querySelector('.main-nav');
+document.addEventListener('click', (e) => {
+  if (!menu.contains(e.target)) {
+    headerNav.classList.add('is-closed');
+    headerNav.classList.remove('is-opened');
+    body.classList.remove('scroll-lock');
+  }
+});
 
 export {onNavToggle};
